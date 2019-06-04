16e885e3ffa389ea772fe234c713f9e866a9fe6d
g0dReVeN <jgovend@student.wethinkcode.co.za>
2019-06-04 11:06:06 +0200

    updated carbon

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'TB SAMPLE DATABASE'),
+    'name' => env('APP_NAME', 'TB SAMPLE MANAGER'),
 
     /*
     |--------------------------------------------------------------------------
